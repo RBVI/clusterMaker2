34292966a42dfb8030288b09631b69e3a622f67e
Abhiraj Tomar <abhirajtomar@gmail.com>
2013-09-23 16:04:44 -0700

    minor modifications

--- src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/algorithms/attributeClusterers/hopach/types/Hopachable.java
+++ src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/algorithms/attributeClusterers/hopach/types/Hopachable.java
@@ -1,3 +1,4 @@
+	
 package edu.ucsf.rbvi.clusterMaker2.internal.algorithms.attributeClusterers.hopach.types;
 
 import edu.ucsf.rbvi.clusterMaker2.internal.algorithms.attributeClusterers.Clusters;
@@ -9,26 +10,25 @@ import edu.ucsf.rbvi.clusterMaker2.internal.algorithms.attributeClusterers.Clust
  *
  */
 public interface Hopachable extends Subsegregatable {
-	
+
 	// Split elements into partitions
 	// A class implementing Hopachable is free to choose the best k, and can use simply choose k = argmin_k MSS
 	//   by calling a separately provided functions to calculate min MSS
 	Clusters split(boolean forceSplit);
-	
+
 	// Return a matrix with distances from each element to each cluster
 	double[][] segregations(Clusters clusters);
-	
+
 	// Return a matrix with distances between clusters (required for ordering of clusters)
 	double[][] separations(Clusters clusters);
-	
+
 	// Collapse a pair of clusters indexed by i and j, given a split
 	Clusters collapse(int i, int j, Clusters clusters);
-	
+
 	// Order elements (elements of the same class should belong together and classes can be rearranged)
 	int[] order(Clusters clusters);
-	
+
 	// Subset
-	@Override
 	Hopachable subset(int[] index);
-	
+
 }
