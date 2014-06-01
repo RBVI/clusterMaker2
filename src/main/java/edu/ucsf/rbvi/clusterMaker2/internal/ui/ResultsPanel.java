0ff0812b026b6ea71651c2d879f5e6b114b3b276
Abhiraj Tomar <abhirajtomar@gmail.com>
2014-06-01 13:18:35 -0400

    Basic code seems ready, need the image part now

--- src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/ui/ResultsPanel.java
+++ src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/ui/ResultsPanel.java
@@ -4,20 +4,24 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Image;
 import java.text.NumberFormat;
 import java.util.Collections;
 import java.util.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
 
 import edu.ucsf.rbvi.clusterMaker2.internal.algorithms.NodeCluster;
 
@@ -28,12 +32,34 @@ import org.cytoscape.view.model.CyNetworkView;
 
 public class ResultsPanel extends JPanel implements CytoPanelComponent{
 
+	private static final long serialVersionUID = 868213052692609076L;
+	
 	public final List<NodeCluster> clusters;
 	private final CyNetwork network;
 	private CyNetworkView networkView;
 	private final int resultId;
 	
+	// table size parameters
+	private static final int graphPicSize = 80;
+	private static final int defaultRowHeight = graphPicSize + 8;
+	
 	private JPanel[] exploreContent;
+	
+	public Component getComponent() {
+		return this;
+	}
+
+	//@Override
+	public CytoPanelName getCytoPanelName() {
+		return CytoPanelName.EAST;
+	}
+
+	//@Override
+	public Icon getIcon() {
+		URL iconURL = MCODEResources.getUrl(ImageName.LOGO_SMALL);
+		return new ImageIcon(iconURL);
+	}
+	
 	
 	/**
 	 * Constructor for the Results Panel which displays the clusters in a
@@ -111,10 +137,13 @@ public class ResultsPanel extends JPanel implements CytoPanelComponent{
 			table.setIntercellSpacing(new Dimension(0, 4)); // gives a little vertical room between clusters
 			table.setFocusable(false); // removes an outline that appears when the user clicks on the images
 
+			
+			/*
 			// Ask to be notified of selection changes.
 			ListSelectionModel rowSM = table.getSelectionModel();
 			rowSM.addListSelectionListener(new ResultsPanel.TableRowSelectionHandler());
-
+			*/
+			
 			JScrollPane tableScrollPane = new JScrollPane(table);
 			tableScrollPane.getViewport().setBackground(Color.WHITE);
 
@@ -130,8 +159,8 @@ public class ResultsPanel extends JPanel implements CytoPanelComponent{
 		}
 
 		public void update(final NodeCluster cluster, final int row) {
-			final StringBuffer details = getClusterDetails(cluster);
-			table.setValueAt(details, row, 1);
+			final StringBuffer score = getClusterScore(cluster);
+			table.setValueAt(score, row, 1);
 		}
 
 		JTable getTable() { 
@@ -192,5 +221,73 @@ public class ResultsPanel extends JPanel implements CytoPanelComponent{
 		}
 	}
 	
+	/**
+	 * A text area renderer that creates a line wrapped, non-editable text area
+	 */
+	private static class JTextAreaRenderer extends JTextArea implements TableCellRenderer {
+
+		int minHeight;
+
+		/**
+		 * Constructor
+		 * 
+		 * @param minHeight
+		 *            The minimum height of the row, either the size of the
+		 *            graph picture or zero
+		 */
+		public JTextAreaRenderer(int minHeight) {
+			this.setLineWrap(true);
+			this.setWrapStyleWord(true);
+			this.setEditable(false);
+			this.setFont(new Font(this.getFont().getFontName(), Font.PLAIN, 11));
+			this.minHeight = minHeight;
+		}
+
+		/**
+		 * Used to render a table cell. Handles selection color and cell height
+		 * and width. Note: Be careful changing this code as there could easily
+		 * be infinite loops created when calculating preferred cell size as the
+		 * user changes the dialog box size.
+		 * 
+		 * @param table Parent table of cell
+		 * @param value Value of cell
+		 * @param isSelected True if cell is selected
+		 * @param hasFocus True if cell has focus
+		 * @param row The row of this cell
+		 * @param column The column of this cell
+		 * @return The cell to render by the calling code
+		 */
+		public Component getTableCellRendererComponent(JTable table,
+													   Object value,
+													   boolean isSelected,
+													   boolean hasFocus,
+													   int row,
+													   int column) {
+			StringBuffer sb = (StringBuffer) value;
+			this.setText(sb.toString());
+
+			if (isSelected) {
+				this.setBackground(table.getSelectionBackground());
+				this.setForeground(table.getSelectionForeground());
+			} else {
+				this.setBackground(table.getBackground());
+				this.setForeground(table.getForeground());
+			}
+
+			// Row height calculations
+			int currentRowHeight = table.getRowHeight(row);
+			int rowMargin = table.getRowMargin();
+			this.setSize(table.getColumnModel().getColumn(column).getWidth(), currentRowHeight - (2 * rowMargin));
+			int textAreaPreferredHeight = (int) this.getPreferredSize().getHeight();
+
+			// JTextArea can grow and shrink here
+			if (currentRowHeight != Math.max(textAreaPreferredHeight + (2 * rowMargin), minHeight + (2 * rowMargin))) {
+				table.setRowHeight(row, Math
+						.max(textAreaPreferredHeight + (2 * rowMargin), minHeight + (2 * rowMargin)));
+			}
+
+			return this;
+		}
+	}
 	
 }
