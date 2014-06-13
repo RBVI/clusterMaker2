8c30fb9ee2f7b583c41f5e4021f93f2ba333c37c
Abhiraj Tomar <abhirajtomar@gmail.com>
2014-06-12 20:09:17 -0700

    added the code to fetch clusters

--- src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/ui/ResultsPanelTask.java
+++ src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/ui/ResultsPanelTask.java
@@ -1,10 +1,17 @@
 package edu.ucsf.rbvi.clusterMaker2.internal.ui;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
+import org.cytoscape.application.swing.CytoPanelComponent;
 import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
 import org.cytoscape.model.CyTable;
 import org.cytoscape.model.CyTableUtil;
+import org.cytoscape.service.util.CyServiceRegistrar;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.TaskMonitor;
@@ -17,6 +24,7 @@ import edu.ucsf.rbvi.clusterMaker2.internal.api.ClusterManager;
 import edu.ucsf.rbvi.clusterMaker2.internal.api.ClusterResults;
 import edu.ucsf.rbvi.clusterMaker2.internal.api.ClusterTaskFactory;
 import edu.ucsf.rbvi.clusterMaker2.internal.api.ClusterViz;
+import edu.ucsf.rbvi.clusterMaker2.internal.utils.ModelUtils;
 
 public class ResultsPanelTask extends AbstractTask implements ClusterViz, ClusterAlgorithm {
 
@@ -27,23 +35,28 @@ public class ResultsPanelTask extends AbstractTask implements ClusterViz, Cluste
 	public static String CLUSTERNAME = "Create Results Panel from Clusters";
 	public static String CLUSTERSHORTNAME = "clusterResultsPanel";
 	public ResultsPanel resultsPanel;
+	private final CyServiceRegistrar registrar;
 	
 	public final List<NodeCluster> clusters;
 	private CyNetworkView networkView;
 	
+	public boolean createFlag;
+	
 	@Tunable(description="Network to look for cluster", context="nogui")
 	public CyNetwork network = null;
 	
-	public ResultsPanelTask(CyNetwork network, ClusterManager manager,List<NodeCluster> clusters) {
-		this(manager, true);
+	public ResultsPanelTask(CyNetwork network, ClusterManager manager,List<NodeCluster> clusters, boolean createFlag) {
+		this(manager, true,createFlag);
 		this.network = network;
 	}
 
 	
-	public ResultsPanelTask( ClusterManager manager, boolean available) {
+	public ResultsPanelTask( ClusterManager manager, boolean available, boolean createFlag) {
 		this.manager = manager;
+		this.createFlag = createFlag;
 		networkView = manager.getNetworkView();
 		
+		registrar = manager.getService(CyServiceRegistrar.class);
 		this.clusters = getClusters();
 		checkForAvailability = available;
 		if (network == null)
@@ -52,17 +65,46 @@ public class ResultsPanelTask extends AbstractTask implements ClusterViz, Cluste
 	}
 	
 	public List<NodeCluster> getClusters(){
-		List<NodeCluster> clusters = null;
+		List<NodeCluster> clusters = new ArrayList<NodeCluster>();
+		clusterAttribute =
+				network.getRow(network, CyNetwork.LOCAL_ATTRS).get(ClusterManager.CLUSTER_ATTRIBUTE, String.class);
 		
+		//Create a temporary cluster map
+		Map<Integer, List<CyNode>> clusterMap = new HashMap<Integer, List<CyNode>>();
+		for (CyNode node: (List<CyNode>)network.getNodeList()) {
+			// For each node -- see if it's in a cluster.  If so, add it to our map
+			if (ModelUtils.hasAttribute(network, node, clusterAttribute)) {
+				
+				Integer cluster = network.getRow(node).get(clusterAttribute, Integer.class);
+				if (!clusterMap.containsKey(cluster))
+					clusterMap.put(cluster, new ArrayList<CyNode>());
+				clusterMap.get(cluster).add(node);				
+			}			
+		}
+		
+		for(int clustNum : clusterMap.keySet()){
+			NodeCluster cluster = new NodeCluster(clusterMap.get(clustNum));
+			cluster.setClusterNumber(clustNum);
+			
+			clusters.add(cluster);
+		}
 		return clusters;
 		
 	}
 	
 
 	public void run(TaskMonitor monitor) {
-		monitor.setTitle("Creating a new results panel with cluster results");
-		
-		resultsPanel = new ResultsPanel(clusters, network,networkView,manager,monitor);
+		if (createFlag){
+			monitor.setTitle("Creating a new results panel with cluster results");
+			
+			resultsPanel = new ResultsPanel(clusters, network,networkView,manager,monitor);
+			registrar.registerService(resultsPanel, CytoPanelComponent.class, new Properties());
+			
+		}
+		else{
+			//need to retrieve the saved resultsPanel to unregister it
+			registrar.unregisterService(resultsPanel, CytoPanelComponent.class);
+		}
 		
 	}
 
