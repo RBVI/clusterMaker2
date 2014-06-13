d63204573c28d8cef8fad729d10de556e76b78fb
Abhiraj Tomar <abhirajtomar@gmail.com>
2014-06-13 09:53:30 -0700

    basic code for destroy task factory

---
+++ src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/ui/DestroyResultsPanelTaskFactory.java
@@ -0,0 +1,70 @@
+package edu.ucsf.rbvi.clusterMaker2.internal.ui;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.work.TaskIterator;
+
+import edu.ucsf.rbvi.clusterMaker2.internal.api.ClusterManager;
+import edu.ucsf.rbvi.clusterMaker2.internal.api.ClusterViz;
+import edu.ucsf.rbvi.clusterMaker2.internal.api.ClusterVizFactory;
+import edu.ucsf.rbvi.clusterMaker2.internal.api.ClusterTaskFactory.ClusterType;
+
+public class DestroyResultsPanelTaskFactory implements ClusterVizFactory {
+
+	ClusterManager clusterManager;
+	boolean checkAvailable;
+	
+	public DestroyResultsPanelTaskFactory(ClusterManager clusterManager, boolean checkAvailable) {
+		this.clusterManager = clusterManager;
+		this.checkAvailable = checkAvailable;
+	}
+
+	public String getShortName() {
+		if (checkAvailable) {
+			return ResultsPanelTask.CLUSTERSHORTNAME;
+		} else {
+			return null; 
+		}
+	}
+
+	public String getName() {
+		if (checkAvailable) {
+			return ResultsPanelTask.CLUSTERNAME;
+		} else {
+			return null; 
+		}
+	}
+
+	public ClusterViz getVisualizer() {
+		return null;
+	}
+
+	public boolean isAvailable(CyNetwork network) {
+		if (!checkAvailable)
+			return true;
+
+		if (network == null)
+			return false;
+
+		return ResultsPanelTask.isReady(network, clusterManager);
+	}
+
+	public List<ClusterType> getTypeList() {
+		return Collections.singletonList(ClusterType.UI);
+		
+	}
+
+	public TaskIterator createTaskIterator() {
+		return new TaskIterator(new ResultsPanelTask(clusterManager, checkAvailable, false));
+
+	}
+
+	public boolean isReady() {
+		if (!checkAvailable)
+			return true;
+		return ResultsPanelTask.isReady(clusterManager.getNetwork(), clusterManager);
+	}
+
+}
