e8d12c35b9abc11794bc7f7a378146d5c6ac89c0
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-04-05 14:54:55 +0200

    Started implementing the multiple value algorithms - find a refactor to the code duplication

--- src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/algorithms/ranking/advanced/MultipleNodeEdgeMultiplum.java
+++ src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/algorithms/ranking/advanced/MultipleNodeEdgeMultiplum.java
@@ -10,11 +10,14 @@ import org.cytoscape.work.ContainsTunables;
 import org.cytoscape.work.TaskMonitor;
 import org.cytoscape.work.Tunable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MultipleNodeEdgeMultiplum extends AbstractTask implements Rank {
     private List<NodeCluster> clusters;
     private ClusterManager manager;
+    private List<String> nodeAttributes;
+    private List<String> edgeAttributes;
     final public static String NAME = "Create rank from multiple nodes and edges (multiply sum)";
     final public static String SHORTNAME = "MNEMrank";
 
@@ -53,10 +56,38 @@ public class MultipleNodeEdgeMultiplum extends AbstractTask implements Rank {
 
     @Override
     public void run(TaskMonitor taskMonitor) {
+        taskMonitor.setProgress(0.0);
         taskMonitor.setTitle("Multiple Node Edge Multiplum ranking of clusters");
         taskMonitor.showMessage(TaskMonitor.Level.INFO, "Creating clusters...");
+        taskMonitor.setProgress(10.0);
         clusters = ClusterUtils.createClusters(network);
+        taskMonitor.setProgress(50.0);
+
+        nodeAttributes = context.getSelectedNodeAttributes();
+        edgeAttributes = context.getSelectedEdgeAttributes();
+        List<Double> scoreList = new ArrayList<>();
+
+        for (String nodeAttr : nodeAttributes) {
+            addNodeScoreToColumn(nodeAttr, taskMonitor, scoreList);
+        }
+
+        taskMonitor.setProgress(75.0);
+
+        for (String edgeAttr : edgeAttributes) {
+            addEdgeScoreToColumn(edgeAttr, taskMonitor, scoreList);
+        }
+
+        taskMonitor.setProgress(100.0);
+
         taskMonitor.showMessage(TaskMonitor.Level.INFO, "Done...");
+    }
+
+    private void addNodeScoreToColumn(String nodeAttr, TaskMonitor taskMonitor, List<Double> scoreList) {
+
+    }
+
+    private void addEdgeScoreToColumn(String edgeAttr, TaskMonitor taskMonitor, List<Double> scoreList) {
+
     }
 
     public static boolean isReady(CyNetwork network, ClusterManager manager) {
