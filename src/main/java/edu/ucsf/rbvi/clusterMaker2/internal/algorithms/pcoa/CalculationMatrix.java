b8c9a66095a8a352e1075a2eb6bc10682881c349
SupunArunoda <supunarunoda@gmail.com>
2016-07-27 22:26:58 +0530

    Some Changes in tSNE

--- src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/algorithms/pcoa/CalculationMatrix.java
+++ src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/algorithms/pcoa/CalculationMatrix.java
@@ -167,60 +167,14 @@ distancematrix.writeMatrix("distancematrix.txt");
 		return G;
 	}
 
-	/*
-	//calculate Gowern's matrix
-	public double[][] getGowernsMatrix(){
-		System.out.println("Started calculating gowerns matrix ");
-		//set ones matrix with row vector
 		
-		double ones[][]=new double[rows][rows];
-		for(int i=0;i<ones.length;i++){
-			for(int j=0;j<ones.length;j++){
-				if(j==0){
-					ones[i][j]=1;	}
-			}}
-		
-		//create unit matrix with row*row dimension
-		double unitmatrix[][]=new double[rows][rows];
-		for(int i=0;i<rows;i++){
-			for(int j=0;j<rows;j++){
-				if(i==j){
-					unitmatrix[i][j]=1;
-				}else{
-					unitmatrix[i][j]=0;
-					}}}
-	
-		//calculate matrixA
-		double matrixA[][]=new double[rows][columns]; 
-		for(int i=0;i<rows;i++){
-			for(int j=0;j<columns;j++){
-				matrixA[i][j]=-0.5*Math.pow(data[i][j], 2);
-			}}
-		
-		//calculate matrixG	
-		double multimatrix[][]=multiplyByMatrix(ones, transposeMatrix(ones));
-		double tempmatrix[][]=new double[rows][rows];
-		for(int i=0;i<rows;i++){
-			for(int j=0;j<rows;j++){
-				tempmatrix[i][j]=unitmatrix[i][j]-(multimatrix[i][j])/rows;
-			}}
-
-		double matrixG[][]=multiplyByMatrix(multiplyByMatrix(tempmatrix, matrixA), tempmatrix);
-		System.out.println("Finished calculating gowerns matrix ");
-	return matrixG;
-	}
-	*/
-	
 	
 	public double[] eigenAnalysis(){
 		System.out.println("Getting Gowers Matrix");
 		Matrix G = getGowersMatrix();
 		System.out.println("Done Getting Gowers Matrix");
 		G.writeMatrix("Gowers.txt");
-		/*
-		double eigenvector[][]=G.eigenVectors();
-		double eigenvalues[]=G.eigenValues(true);	
-		*/
+		
 		eigen_vectors=G.eigenVectors();
 		 eigen_values=G.eigenValues(true);	
 
@@ -231,93 +185,7 @@ distancematrix.writeMatrix("distancematrix.txt");
 			System.out.println("  "+d);
 		}
 
-		/*int idx_size=0;//for set idx length 
-		double tempeigen[]=new double[eigenvalues.length];
-		for(int i=0;i<eigenvalues.length;i++){
-			if(Math.abs(eigenvalues[i])>EPSILON){
-				tempeigen[i]=1;
-				idx_size++;
-			}}*/
-
 		
-		//calculate idx value from eigen values
-		/*double idx[]=matrixReverse(tempeigen);
-		int count=1;
-		//double idx[]=new double[idx_size];
-		for(int i=0;i<idx.length;i++){
-			if(idx[i]!=0){
-				idx[i]=count;
-			}
-			count++;
-		}*/
-		
-		
-		//discard eigen values
-		/*double reverseeigen[]=new double[eigenvalues.length];
-		int j=0;
-		for(int i=eigenvalues.length-1;i>=0;i--){
-			reverseeigen[j]=eigenvalues[i];
-			j++;
-		}*/
-		/*eigen_values=new double[idx.length];
-		for(int i=0;i<reverseeigen.length;i++){
-			for(j=0;j<idx.length;j++){
-				if(i+1==idx[j]){
-					eigen_values[j]=reverseeigen[i];
-				}
-			}
-		}
-
-		eigenVectors = distancematrix.copy();
-
-		for(int row=0;row<eigenVectors.nRows();row++){
-			for(int col=0;col<eigenVectors.nColumns();col++){
-				for(int k=0;k<idx.length;k++){
-					if(col+1==idx[k]){
-						eigenVectors.setValue(row, col, eigenvector[row][col]);
-					}
-				}
-			}
-		}
-		
-		for(int row=0;row<eigenVectors.nRows();row++){
-			for(int col=0;col<eigenVectors.nColumns();col++){
-				if(eigenVectors.getValue(row,col)==0 && col!=eigenVectors.nRows()-1){
-					double temp=eigenVectors.getValue(row,col);
-					eigenVectors.setValue(row, col, eigenVectors.getValue(row,col+1));
-					eigenVectors.setValue(row, col+1, temp);
-				}
-			}
-		}
-		//calculate final length of eigen values
-		int length_count=0;
-		for(int i=0;i<eigen_values.length;i++){
-			if(eigen_values[i]==0 && i!=eigen_values.length-1){//shift all zeros to back
-				double temp=eigen_values[i];
-				eigen_values[i]=eigen_values[i+1];
-				eigen_values[i+1]=temp;
-			}
-				if(eigen_values[i]!=0){
-					length_count+=1;//count to reduced eigen val length
-				}
-			}
-
-
-		//need n-1 axes for n objects
-		if(length_count>eigen_values.length-1){
-			for(int i=0;i<eigen_values.length;i++){
-				if(eigen_values[i]!=0 && length_count<i+length_count-1){
-					eigen_values[i]=0;
-				}
-			}
-			for(int row=0;row<eigenVectors.nRows();row++){
-				for(int col=0;col<eigenVectors.nColumns();col++){
-					if(eigenVectors.getValue(row, col)!=0 && length_count<col+length_count-1){
-						eigenVectors.setValue(row, col, 0);
-					}
-				}
-			}
-		}*/
 		return eigen_values;
 	}
 	
@@ -427,108 +295,10 @@ distancematrix.writeMatrix("distancematrix.txt");
 		return matrix;
 	}
 	
-	
-	// handle negative eigen values
-	public double[][] negativeEigenAnalysis(){
-		
-		//neg=0;//for test
-		double negSum=0;
-		for(int i=0;i<eigen_values.length;i++){
-			if(eigen_values[i]<0){
-				negSum+=i+1;
-			}
-		}
-		
-		double temp_min=0;
-		double uppermatrixp[]=new double[2];//the size can be changed
-		double columnmatrix[]=new double[2];//the size can be changed
-		double converedmatrix[][]=new double[2][2];//the size can be changed
-		if(negSum>0 && neg==2){//should include && correct value check matlab
-
-			// XXX: What correction is this?  Looks kind of like Cailliez, but not quite.  Why not
-			// just use Lingoes correction: (D' = -0.5*D^2 - c1), which is probably much easier to compute, and we
-			// won't lose track of our labels
-			/*
-			for(int i=0;i<eigen_values.length;i++){
-				if(eigen_values[i]<temp_min){
-					temp_min=eigen_values[i];
-				}
-			}
-			temp_min=Math.abs(temp_min);
-			uppermatrixp=getUpperMatrixInVector(distancematrix);
-			columnmatrix=new double[uppermatrixp.length];
-			for(int i=0;i<uppermatrixp.length;i++){
-				columnmatrix[i]=Math.sqrt((Math.pow(uppermatrixp[i],2)+2*temp_min));
-			}
-
-			// TODO: How do we track the labels -- e.g. what nodes go with what eigenvectors??
-			converedmatrix=convertColumntoMatrix(columnmatrix);
-			CalculationMatrix calc=new CalculationMatrix(converedmatrix.length, converedmatrix.length, converedmatrix,0,0,1);
-			eigen_values=calc.getEigen_values();
-			combine_array=calc.getCombine_array();
-			scores=calc.getScores();
-		*/
-
-		} else if(negSum>0 && neg<1){//discard negative eigen values
-			
-			int count=0;
-			for(int i=0;i<eigen_values.length;i++){
-				if(eigen_values[i]<0){
-					eigen_values[i]=0;
-				}else{
-					count+=1;
-				}
-			}
-			for(int row=0;row<eigenVectors.nRows();row++){
-				for(int col=0;col<eigenVectors.nColumns();col++){
-					if(col+1==count){
-						eigenVectors.setValue(row, col, 0);
-					}}}
-			
-			for(int i=0;i<combine_array.length;i++){
-				for(int j=0;j<2;j++){//combine array column length is 2. Because it declares using two column matrixes
-					if(i+1==count){
-						combine_array[i][j]=0;
-					}}}
-		}
-		return combine_array;
-	}
-	
-	
-	//scale eigen vectors
-	public CyMatrix scaleEigenVectors(){
-		double temp_eigen[][]=new double[eigen_values.length][eigen_values.length];
-		
-		if(scale){//default value
-			// Use Matrix for this?
-			for(int i=0;i<eigen_values.length;i++){
-				for(int j=0;j<eigen_values.length;j++){
-					if(j==0){
-						temp_eigen[i][j]=Math.pow(Math.abs(eigen_values[i]), 0.5);	
-					}else{
-						temp_eigen[i][j]=0;
-					}
-				}
-			}
-			
-			temp_eigen=transposeMatrix(temp_eigen);
-			CyMatrix multi_matrix=eigenVectors.copy();
-			for(int i=0;i<eigen_values.length;i++){
-				for(int j=0;j<temp_eigen.length;j++){
-					multi_matrix.setValue(i, j, temp_eigen[0][j]);
-				}
-			}
-			Matrix scorematrix = eigenVectors.multiplyMatrix(multi_matrix);
-			// Does this work or do we still mess up the correspondence between labels and values?
-			return eigenVectors.copy(scorematrix);
-		} else {
-			return eigenVectors;
-			// scores=eigenvectors;
-		}
-	}
 
 	private static DecimalFormat scFormat = new DecimalFormat("0.###E0");
 	private static DecimalFormat format = new DecimalFormat("0.###");
+	
 	String printMatrix(DoubleMatrix2D mat) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("matrix("+mat.rows()+", "+mat.columns()+")\n");
