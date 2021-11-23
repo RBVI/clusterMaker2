8973de78a51cb2bff02b2c4133706268ed197f3d
Scooter Morris <scooter@cgl.ucsf.edu>
2021-11-23 15:15:53 -0800

    Added longDescription to the new algorithms

--- src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/algorithms/networkClusterers/Multilevel/MultilevelClusterTaskFactory.java
+++ src/main/java/edu/ucsf/rbvi/clusterMaker2/internal/algorithms/networkClusterers/Multilevel/MultilevelClusterTaskFactory.java
@@ -28,7 +28,35 @@ package edu.ucsf.rbvi.clusterMaker2.internal.algorithms.networkClusterers.Multil
 		
 		@Override
 		public String getLongDescription() {
-			return "";
+			return "This function implements the multi-level "+
+			       "modularity optimization algorithm for finding "+
+			       "community structure, see Blondel, V. D., "+
+			       "Guillaume, J.-L., Lambiotte, R., & Lefebvre, "+
+			       "E. (2008). Fast unfolding of communities "+
+			       "in large networks. Journal of Statistical "+
+			       "Mechanics: Theory and Experiment, 10008(10), 6. "+
+			       "https://doi.org/10.1088/1742-5468/2008/10/P10008 "+
+			       "for the details (preprint: "+
+			       "http://arxiv.org/abs/0803.0476). The algorithm "+
+			       "is sometimes known as the 'Louvain' algorithm. "+
+             " "+
+             "The algorithm is based on the modularity measure and a hierarchical "+
+             "approach. Initially, each vertex is assigned to a community on its own. In "+
+             "every step, vertices are re-assigned to communities in a local, greedy "+
+             "way: in a random order, each vertex is moved to the community with which "+
+             "it achieves the highest contribution to modularity. When no vertices "+
+             "can be reassigned, each community is considered a vertex on its own, "+
+             "and the process starts again with the merged communities. The process "+
+             "stops when there is only a single vertex left or when the modularity "+
+             "cannot be increased any more in a step. "+
+             " "+
+             "The resolution parameter gamma allows finding communities at different "+
+             "resolutions. Higher values of the resolution parameter typically result in "+
+             "more, smaller communities. Lower values typically result in fewer, larger "+
+             "communities. The original definition of modularity is retrieved when "+
+             "setting gamma=1. Note that the returned modularity value is calculated "+
+             "using the indicated resolution parameter. See igraph_modularity() for "+
+             "more details. This function was contributed by Tom Gregorovic. ";
 		}
 
 		@Override
